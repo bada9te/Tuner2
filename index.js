20dc097558ff3a3560339e0cee54f50709bf2834
bada9te <bogdantelep@gmail.com>
2025-04-06 17:13:59 +0300

    override BigInt.toJSON()

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const {MainCustomExtractor} = require("./custom-audio-extractors/mainExtractor")
 
 require('dotenv').config();
 
+BigInt.prototype.toJSON = function () { return Number(this) }
+
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, 'GuildVoiceStates'] });
 
